f3a582f82f4fb9888aa10b44ecbb508a525db2ff
ShaunakArora <shaunakaroradpsv@gmail.com>
2025-05-09 15:11:03 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,13 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
-
-/** google_map js **/
 function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var mapProp = {
+                center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                zoom: 18,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
 }
